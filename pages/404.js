3efc11926db143f24a406eca4f9f32e20ffc1638
neyoa <itstomsmart@gmail.com>
2021-06-21 17:59:25 +0100

    Add better 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,9 @@
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <div id="container" onContextMenu={(e) => e.preventDefault()}>
+      <main id="main">
+        <h1 className="text-large font-bold leading-tight bg-gradient-to-br bg-clip-text text-transparent from-purple-700 to-pink-600">404</h1>
+        <h2 className="text-3xl font-semibold bg-gradient-to-br bg-clip-text text-transparent from-purple-700 to-pink-600">Page not found!</h2>
+      </main>
+    </div>)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,32 +3,35 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    colors: {
-      light: {
-        'text': '#23272a',
-        'background': '#ffffff',
-        'tooltip-background': '#efefef',
-      },
-      dark: {
-        'text': '#eaeaea',
-        'background': '#23272a',
-        'tooltip-background': '#2c2f33',
-      },
-      contact: {
-        'start': '#4ade80',
-        'end': '#06b6d4',
-      },
-      'primary': '#f53678',
-      'secondary': '#0fc0fc',
-      'tooltip-background': '#50e3c2',
-      'transparent': 'transparent',
-    },
     extend: {
+      colors: {
+        light: {
+          'text': '#23272a',
+          'background': '#ffffff',
+          'tooltip-background': '#efefef',
+        },
+        dark: {
+          'text': '#eaeaea',
+          'background': '#23272a',
+          'tooltip-background': '#2c2f33',
+        },
+        contact: {
+          'start': '#4ade80',
+          'end': '#06b6d4',
+        },
+        'primary': '#f53678',
+        'secondary': '#0fc0fc',
+        'tooltip-background': '#50e3c2',
+        'transparent': 'transparent',
+      },
       inset: {
         'tooltip': '120%'
       },
       transitionDuration: {
         '1500': '1.5s'
+      },
+      fontSize: {
+        'large': '16rem',
       },
     },
   },
